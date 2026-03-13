2afd2aecfb7ae947969b610efacc60864d979b1b
IvyOwusu1 <ivyowusua13@gmail.com>
2026-03-13 14:04:16 +0000

    Improved UI and added unit test for homepage endpoint

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import pytest
+
+# allow pytest to find app.py in the project root
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
